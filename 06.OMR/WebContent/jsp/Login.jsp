43b40d967efddc2bfc4bedaa968d4606fdaa4c0d
juun12 <chonjang2255@naver.com>
2021-12-06 18:38:08 +0900

    1111

--- 06.OMR/WebContent/jsp/Login.jsp
+++ 06.OMR/WebContent/jsp/Login.jsp
@@ -1,12 +1,116 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%
+
+%>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
 <title>Login</title>
+<style >
+body {
+	font:14px/1.5 "Noto Sans KR", sans-serif;
+	color: #888;
+	overflow-x:hidden;
+}
+
+.blind {
+	display: none;
+}
+
+a, a:link {
+	text-decoration: none;
+	color: inherit; /* 부모 요소로부터 색상을 상속받아 사용한다 */
+}
+ul {
+	   list-style:none;
+  	   padding-left:0px;
+}
+</style>
+<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="js/index.js"></script>
+<script type="text/javascript">
+	/* 수험자번호 글자수제한 */
+	function maxLenthCheck(e) {
+	  if(e.value.length > e.maxLength)  {
+	    e.value  = e.value.slice(0, e.maxLength);
+	  }//if
+}//function
+
+//EnterKey금지
+document.addEventListener('keydown', function(event) {
+	  if (event.keyCode === 13) {
+	    event.preventDefault();
+	  };
+	}, true);
+$(document).ready(function(){ 
+	$("#adminBtn").click(function() {
+		var studentid = $("#studentid").val();
+		var studentname = $("#studentname").val();
+		
+		if (!studentid || !studentname) {
+			swal("관리자만 접근 가능합니다.");
+		} else {
+			$.ajax({ //ajax 실행
+				url : "cs.do",
+				type : "POST",
+				data : {
+					studentid : studentid,
+					studentname : studentname
+				}, 
+				success : function(result) {
+					if (result == 0) {
+							swal("관리자 정보가 잘못됐습니다.");
+							$("#studentid").focus();
+					} else if (result == 3) {
+							if(confirm("관리자 페이지로 이동하시겠습니까?") == false){
+								swal("취소되었습니다.");
+							}else {
+								location.href="Manager.jsp";
+							}
+					}//if
+				}//success
+			});//ajax 
+		}//if
+	});//click
+})//ready
+</script>
 </head>
 <body>
+	<div class="container">
+		<div id="wrap">
+		<form method="post" action="studentExam.do" id="studentform" onsubmit="return fnSubmit()" onreset="return fnReset()">
+			<table>
+				<div>
+				<img src="images/visual_bg.PNG" alt="omr" />
+				</div>
+				<ul>
+					<li><label for="studentname">수험자 이름</label></li>
+					<li>
+						<input type="text" class="input" placeholder="수험자 이름"name="studentname" id="studentname" maxlength="6">
+					</li>
+				</ul>
+				<ul>
+					<li><label for="studentid">수험자 번호</label></li>
+					<li>
+						<input type="number" class="input" placeholder="수험자 번호" name="studentid" id="studentid" 
+						 maxlength="5" autofocus="autofocus" oninput="maxLenthCheck(this);"/>
+					</li>
+				</ul>
+
+				<div>			
+					<input type="submit" class="submitBtn" id="submitBtn" value="응시하기" />
+					<input type="reset" class="resetBtn" id="resetBtn" value="초기화" />
+					<input type="button" class="plustBtn" id="plusBtn" value="추가접수" />
+					<input type="button" class="adminBtn" id="adminBtn" value="관리자" />
+					
+				</div>
+
+			 </table>
+		</form>
+		</div>
+	</div>	<!-- class: container  -->
 
 </body>
 </html>
--- 06.OMR/src/com/hanul/DAO/OmrDAO.java
+++ 06.OMR/src/com/hanul/DAO/OmrDAO.java
@@ -9,10 +9,10 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 import com.hanul.DTO.StudentDTO;
+import com.hanul.dto.BoardDTO;
+import com.hanul.dto.SearchDTO;
 
 public class OmrDAO {
-	//기존 JDBC 모델에서는 Connection(연결객체)을 먼저 만들었지만,
-	//myBatis 경우 SqlSessionFactoty 객체를 먼저 생성
 	private static SqlSessionFactory sqlMapper;
 	static {
 		try {
@@ -25,74 +25,70 @@ public class OmrDAO {
 		}
 	}//초기화 블럭
 	
-//---------------------------------------기근태 시작---------------------------------------//
-	
-	//수험생 등록
-	public int memberInsert(StudentDTO dto) {
-		//SqlSessionFactory(sqlMapper)에서 session 활성화
-		SqlSession session = sqlMapper.openSession();
-		
-		int succ = 0;	//성공여부 판단
-		
-		//insert 작업(SQL문장작성) → Mapper.xml
-		succ = session.insert("sdInsert", dto);
-		
-		session.commit();	//커밋명령
-		
-		session.close();	//session 종료
-		
-		return succ;		//결과를 리턴		
-	}//memberInsert()
-	
-	//전체회원 목록검색
-	public List<StudentDTO> studentSearchAll() {
-		SqlSession session = sqlMapper.openSession();
-		List<StudentDTO> list = null;
-		list = session.selectList("studentSearchAll");
-		session.close();
-		return list;
-	}//memberSearchAll()
-	
-	/*
-	//회원정보 삭제
-	public int memberDelete(String id) {
-		SqlSession session = sqlMapper.openSession();
-		int succ = 0;
-		succ = session.delete("memberDelete", id);
-		session.commit();
-		session.close();
-		return succ;
-	}//memberDelete()
-	
-	//회원정보 검색
-	public MemberDTO getById(String id) {
-		SqlSession session = sqlMapper.openSession();
-		MemberDTO dto = null;
-		dto = session.selectOne("getById", id);
-		session.close();
-		return dto;
-	}//getById()
-	
-	//회원정보 수정
-	public int memberUpdate(MemberDTO dto) {
-		SqlSession session = sqlMapper.openSession();
-		int succ = 0;
-		succ = session.update("memberUpdate", dto);
-		session.commit();
-		session.close();
-		return succ;
-	}//memberUpdate()
-	
-	//조건검색
-	public List<MemberDTO> memberSearch(SearchDTO dto) {
-		SqlSession session = sqlMapper.openSession();
-		List<MemberDTO> list = null;
-		list = session.selectList("memberSearch", dto);
-		session.close();
-		return list;
-	}//memberSearch()
-	
-	---------------------------------------------------------------------기근태 끝 */
-	
-	
-}//class
+	//회원 가입
+		public int login(StudentDTO dto) {
+			//SqlSessionFactory(sqlMapper)에서 session 활성화
+			SqlSession session = sqlMapper.openSession();
+			int succ = 0;
+			succ = session.insert("login", dto); //insert SQL insert 쿼리 : Mapper.xml
+			session.commit();	//커밋
+			session.close();	//session 비활성화
+			return succ;
+		} //login()
+
+		//수험생 체크
+		loginCheck
+		public StudentDTO studentLogin(StudentDTO dto) {
+			SqlSession session = sqlMapper.openSession();  //session 활성화
+			//mybatis에서는 arraylist가 아닌 list를 사용한다
+			//파라미터에 식별자 이름을 적는다. 
+			
+			dto = (StudentDTO) session.selectList("studentLogin", dto);// mapper로 StudentDTO 값을 넘김
+			session.close();
+			return dto;
+
+
+			//② 비지니스 로직
+			BookDAO dao = new BookDAO();
+			
+			-------------------controller
+			
+			
+			int cnt = dao.checkIsbn(dto);
+			
+			//③ 프리젠테이션 로직
+			response.setContentType("text/html; charset=utf-8");
+			PrintWriter out = response.getWriter();
+			if(cnt>0) {
+				out.println("<script>alert('수험자번호 중복!!!'); location.href='javascript:history.go(-1);';</script>");
+			} else {	
+				int succ = dao.insertBook(dto);
+				if(succ > 0) {
+					out.println("<link rel='stylesheet' href='style.css' />");
+					out.println("<div class='bg'>");
+					out.println("<video muted autoplay loop>");
+					out.println("<source src='rem.mp4' type='video/mp4'></video>");
+					out.println("<div class='text'>");
+					out.println("<script>alert('도서목록 입력성공!!!');</script>");
+					out.println("<div class=wrapper><a href='bookMain.html'>도서정보 입력하기</a>");
+					out.println("<br/>");
+					out.println("<a href='gals.do'>전체도서 목록보기</a></div>");
+					out.println("</div>");
+					out.println("</div>");
+				}else {
+					out.println("<link rel='stylesheet' href='style.css' />");
+					out.println("<div class='bg'>");
+					out.println("<video muted autoplay loop>");
+					out.println("<source src='rem.mp4' type='video/mp4'></video>");
+					out.println("<div class='text'>");
+					out.println("<script>alert('도서목록 입력실패!!!');</script>");
+					out.println("<div class=wrapper><a href='bookMain.html'>도서정보 입력하기</a>");
+					out.println("<br/>");
+					out.println("<a href='gals.do'>전체도서 목록보기</a></div>");
+					out.println("</div>");
+					out.println("</div>");
+				}
+			}		
+		}
+	}
+
